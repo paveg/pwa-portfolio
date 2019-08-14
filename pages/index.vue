873d659894b56dcd8b0ab2a5b2589db6cd847870
Ryota Ikezawa <pavegy@gmail.com>
2019-08-14 22:06:23 +0900

    Complete

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,6 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
       <div class="text-center">
         <logo />
         <vuetify-logo />
@@ -18,59 +10,51 @@
           Welcome to the Vuetify + Nuxt.js template
         </v-card-title>
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
           <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
+            Vuetify is a progressive Material Design component framework for
+            Vue.js. It was designed to empower developers to create amazing
+            applications.
           </p>
           <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
+            For more information on Vuetify, check out the
+            <a href="https://vuetifyjs.com" target="_blank"> documentation </a>.
+          </p>
+          <p>
+            If you have questions, please join the official
+            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">
               discord
-            </a>.
+            </a>
+            .
           </p>
           <p>
-            Find a bug? Report it on the github <a
+            Find a bug? Report it on the github
+            <a
               href="https://github.com/vuetifyjs/vuetify/issues"
               target="_blank"
               title="contribute"
             >
               issue board
-            </a>.
+            </a>
           </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
+          <p>
+            Thank you for developing with Vuetify and I look forward to bringing
+            more exciting features in the future.
+          </p>
           <div class="text-xs-right">
             <em><small>&mdash; John Leider</small></em>
           </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
+          <hr class="my-3" />
+          <a href="https://nuxtjs.org/" target="_blank">
             Nuxt Documentation
           </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
+          <br />
+          <a href="https://github.com/nuxt/nuxt.js" target="_blank">
             Nuxt GitHub
           </a>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
+          <v-btn color="primary" nuxt to="/inspire">
             Continue
           </v-btn>
         </v-card-actions>
@@ -79,14 +63,15 @@
   </v-layout>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+<script lang="ts">
+import Vue from "vue"
+import Logo from "~/components/Logo.vue"
+import VuetifyLogo from "~/components/VuetifyLogo.vue"
 
-export default {
+export default Vue.extend({
   components: {
     Logo,
     VuetifyLogo
   }
-}
+})
 </script>
---
+++ pages/profile/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>
+      profile page
+    </h1>
+  </div>
+</template>
